248558d235906d6f4e7dbeb2d75c829f3d16efa8
klpoland <kpunicorn@gmail.com>
2020-05-26 13:45:19 -0400

    Update config.js

--- Provider/config.js
+++ Provider/config.js
@@ -1,7 +1,7 @@
 module.exports.config = () => {
     const config = {
-        clientId: '4be18633-25b6-45e0-a23d-b32af0659cbc',
-        clientSecret: 'bM4k04FR8LRe2m65PxKOlR2DX9S2JMwKrt4YobA4nxQ',
+        clientId: 'd2l_oauth2_client_id',
+        clientSecret: 'd2l_oauth2_client_secret',
         hostUrl: 'https://ugatest2.view.usg.edu',
         accessTokenUrl: 'https://auth.brightspace.com/core/connect/token',
         clientUrl: 'https://testing.appool.org'
